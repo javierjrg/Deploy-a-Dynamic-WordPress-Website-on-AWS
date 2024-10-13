3bd94d1f32dbcee5ff2eda5f6839f97ac8840113
HeH+ <50049728+javierjrg@users.noreply.github.com>
2024-10-13 06:28:25 -0600

    Script Files
    Uploading the script files used on this hands-on project and the architecture reference diagram.

---
+++ install-wordpress.sh
@@ -0,0 +1,94 @@
+# create to root user
+sudo su
+
+# update the software packages on the ec2 instance 
+sudo yum update -y
+
+# create an html directory 
+sudo mkdir -p /var/www/html
+
+# environment variable
+EFS_DNS_NAME=fs-088f78a73fa9ec639.efs.us-east-1.amazonaws.com
+
+# mount the efs to the html directory 
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport "$EFS_DNS_NAME":/ /var/www/html
+
+# Verify the EFS is mounted
+df -h
+
+# install the apache web server, enable it to start on boot, and then start the server immediately
+sudo yum install -y httpd
+sudo systemctl enable httpd 
+sudo systemctl start httpd
+sudo systemctl status httpd
+
+# install php 8 along with several necessary extensions for wordpress to run
+sudo dnf install -y \
+php \
+php-cli \
+php-cgi \
+php-curl \
+php-mbstring \
+php-gd \
+php-mysqlnd \
+php-gettext \
+php-json \
+php-xml \
+php-fpm \
+php-intl \
+php-zip \
+php-bcmath \
+php-ctype \
+php-fileinfo \
+php-openssl \
+php-pdo \
+php-tokenizer
+
+# install the mysql version 8 community repository
+sudo wget https://dev.mysql.com/get/mysql80-community-release-el9-1.noarch.rpm 
+
+# install the mysql server
+sudo dnf install -y mysql80-community-release-el9-1.noarch.rpm 
+sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2023
+sudo dnf repolist enabled | grep "mysql.*-community.*"
+sudo dnf install -y mysql-community-server 
+#
+# start and enable the mysql server
+sudo systemctl start mysqld
+sudo systemctl enable mysqld
+sudo systemctl status mysqld
+
+# set permissions
+sudo usermod -a -G apache ec2-user
+sudo chown -R ec2-user:apache /var/www
+sudo chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {} \;
+sudo find /var/www -type f -exec sudo chmod 0664 {} \;
+chown apache:apache -R /var/www/html 
+
+# download wordpress files
+wget https://wordpress.org/latest.tar.gz
+tar -xzf latest.tar.gz
+sudo cp -r wordpress/* /var/www/html/
+
+# Move into the html directory
+cd /var/www/html && ls
+ls
+
+# create the wp-config.php file
+sudo cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+
+# edit the wp-config.php file
+sudo nano /var/www/html/wp-config.php
+dbName: applicationdb
+Username: awsproject
+PS: AWSProject2024!!!
+Hostname: dev-rds-db.c7mm0akaceev.us-east-1.rds.amazonaws.com
+
+# restart the webserver
+sudo service httpd restart && sudo service httpd status
+
+# Health Check
+/wp-admin/install.php
+200,301,302
+sudo nano /etc/selinux/config
+SELINUX= permissive or disable
